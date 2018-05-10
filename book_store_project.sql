aa564cfdec6bf6c7592da2eec14b9af5089eb8eb
YousefZook <yousefzook@outlook.com>
2018-05-10 15:10:41 +0200

    Refine code shape.

--- book_store_project.sql
+++ book_store_project.sql
@@ -1,4 +1,4 @@
-create schema book_store;
+create schema if not exists book_store;
 use book_store;
 
 create table books(
@@ -90,6 +90,7 @@ insert into categories values ("history");
 insert into categories values ("geography");
 
 DELIMITER ;;
+
 -- when update no_of_copies, check the threshold
 create trigger 	update_quantity_threshold_trigger
 before update on books
@@ -112,13 +113,13 @@ end;;
 
 
 -- if the book is ordered already, update the no_of_copies value
--- create trigger 	insert_order_pk_trigger
--- before insert on store_orders
--- for each row begin
--- 	if(exists (select isbn from store_orders where isbn = new.isbn)) then
--- 		delete from store_orders where isbn = new.isbn;
---     end if;
--- end;;
+create trigger 	insert_order_pk_trigger
+before insert on store_orders
+for each row begin
+ 	if(exists (select isbn from store_orders where isbn = new.isbn)) then
+ 		delete from store_orders where isbn = new.isbn;
+     end if;
+ end;;
 
 
 -- if the user want to update no_of_copies with -ve values, reject
@@ -144,7 +145,38 @@ end;;
 --     update books set no_of_copies=no_of_copies_ordered where isbn=isbn_ordered;
 -- end;;
 
+
+-- Searching procedures which will be called in the front end to support the user with the required information
+CREATE PROCEDURE search_by_isbn (ISBN int)
+BEGIN
+	SELECT * from books where books.isbn = ISBN;
+END;;
+
+CREATE PROCEDURE search_by_title (Title varchar(100))
+BEGIN
+	SELECT * from books where books.title = Title;
+END;;
+
+CREATE PROCEDURE search_by_category (category varchar(20))
+BEGIN
+	SELECT * from books where books.category = category;
+END;;
+
+CREATE PROCEDURE search_by_author (author varchar(100))
+BEGIN
+	SELECT * from authors join books on authors.isbn = books.isbn where authors.author = author;
+END ;;
+
+CREATE PROCEDURE search_by_publisher (publisher varchar(100))
+BEGIN
+	SELECT * from books where books.publisher_name = publisher;
+END ;;
+
 DELIMITER ;
+
+
+
+call search_by_category('art');
 
 select * from store_orders;
 select * from books;
@@ -155,5 +187,19 @@ select * from books;
 insert into publishers values("pubB", "addB");
 insert into publishers values("pub", "add");
 
-insert into books values(2, "B", "pubB", 1000, 5, "art", 10, 30);
-insert into books values(3, "a", "pub", 2000, 9.5, "art", 20, 13);
+insert into books values(1, "C pogramming", "pubB", 2000, 9.5, "art", 20, 50);
+insert into authors values(1,"Denis Richie");
+insert into books values(2, " Do Androids Dream of Electric Sheep?", "pubB", 1000, 5, "science", 10, 30);
+insert into authors values(2," Philip K. Dick");
+insert into books values(3, " Everything I Never Told You", "pub", 2000, 9.5, "History", 20, 50);
+insert into authors values(3,"Celeste Ng");
+insert into books values(4, "Harry Potter", "pubB", 2000, 9.5, "art", 20, 50);
+insert into authors values(4,"J.K Rowling");
+insert into books values(5, "Effective Java", "pub", 2000, 9.5, "art", 20, 50);
+insert into authors values(5,"John");
+-- SET FOREIGN_KEY_CHECKS=0;
+-- SET SQL_SAFE_UPDATES = 0;
+-- delete from books;
+-- delete from authors;
+
+update books set no_of_copies = 10 where isbn = 3;
